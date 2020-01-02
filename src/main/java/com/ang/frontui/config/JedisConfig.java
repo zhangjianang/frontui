e61edad3783988214c2fffcefd4b72a2f2c7b52a
zhangja <zhangja@xiaoshouyi.com>
2020-01-02 19:14:24 +0800

    加入widgets

---
+++ src/main/java/com/ang/frontui/config/JedisConfig.java
@@ -0,0 +1,19 @@
+package com.ang.frontui.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+@Configuration
+public class JedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)  {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setDefaultSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
